dbe277689a2c0d66818ec38859e982ea50dcbdf9
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2018-10-05 06:08:28 -0400

    Added portfolio links

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 const DashboardPage = () => (
-  <div className="content-container">
+  <div>
     <h1>Projects</h1>
-    <a href="https://buzzy-expensify.herokuapp.com/"><img src="/images/expensify.jpg" alt="expensify"/></a>
+    <div className="content-container">
+      <div>
+        <a href="https://buzzy-expensify.herokuapp.com/"><img src="/images/expensify.jpg" alt="expensify"/></a>
+      </div>
+      <div>
+        <a href="https://buzzy-indecision.herokuapp.com/"><img src="/images/indecision.jpg" alt="indecision"/></a>
+      </div>
+      <div>
+        <a href="https://nbbuzzy1.github.io/image-gallery/"><img src="/images/image-gallery.jpg" alt="image-gallery"/></a>
+      </div>
+      <div>
+        <a href="https://nbbuzzy1.github.io/todo-list/"><img src="/images/todo-list.jpg" alt="todo-list"/></a>
+      </div>
+      <div>
+        <a href="https://nbbuzzy1.github.io/color-game/"><img src="/images/color-game.jpg" alt="color-game"/></a>
+      </div>
+      <div>
+        <a href="https://nbbuzzy1.github.io/background-generator/"><img src="/images/background-generator.jpg" alt="color-game"/></a>
+      </div>
+      <div>
+        <a href="https://nbbuzzy1.github.io/startup/"><img src="/images/startup.jpg" alt="startup"/></a>
+      </div>
+      <div>
+        <a href="https://buzzy-robofriends.herokuapp.com/"><img src="/images/robofriends.jpg" alt="robofriends"/></a>
+      </div>
+    </div>
   </div>
 );
 
