a12e1e8e0db8ee5e26a6faabc562bb518ab89d0b
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2018-10-28 07:55:19 -0400

    added github, stack overflow, removed tests

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => (
+  <div className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <img id="reactCourse" src="/images/react-course-completion.jpg"></img>
+      </div>
+    </div>
+  </div>
+);
+
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,8 @@ const Header = () => (
         <Link className="header__title" to="/">
           <h1>Nick Buzzy</h1>
         </Link>
+        <a href="https://github.com/nbbuzzy1" target="_blank"><i id="github" className="fa fa-github-square fa-5x fa-fw"></i> </a>
+        <a href="https://stackoverflow.com/users/10047087/nick-buzzy?tab=profile" target="_blank"><i id="stackOverflow"  className="fa fa-stack-overflow fa-5x fa-fw"></i></a>
       </div>
     </div>
   </header>
